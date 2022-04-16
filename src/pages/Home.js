c01140ebc58ce15fbed31e3ed5cfd4a0a88dab73
Panasun Sunanta <panasunsunanta@gmail.com>
2022-04-16 18:29:36 +0700

    feat: add axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,6 @@
+import React, { useState, useEffect } from 'react';
 import Banner from "../assets/banner.jpg";
-
-const foodMenus = [
-  {
-    image:
-      "https://img.wongnai.com/p/1920x0/2018/12/17/bf69cc77dfb94a5ab6df20ffb0622cd2.jpg",
-    name: "แกงส้มชะอมกุ้ง",
-    price: 150,
-  },
-  {
-    image:
-      "https://tourdefood.net/wp-content/uploads/2020/11/%E0%B8%84%E0%B8%B0%E0%B8%99%E0%B9%89%E0%B8%B2-%E0%B9%80%E0%B8%AB%E0%B9%87%E0%B8%94%E0%B8%AB%E0%B8%AD%E0%B8%A1-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%A1%E0%B8%B1%E0%B8%99%E0%B8%AB%E0%B8%AD%E0%B8%A2-%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%94%E0%B8%B5%E0%B8%95%E0%B9%88%E0%B8%AD%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B8%A0%E0%B8%B2%E0%B8%9E.jpg",
-    name: "ผัดคะน้าเห็ดหอม",
-    price: 130,
-  },
-  {
-    image: "https://i.ytimg.com/vi/9YPqFc5BYWA/maxresdefault.jpg",
-    name: "หมูสามชั้นคั่วพริกเกลือ",
-    price: 200,
-  },
-];
+import axios from 'axios';
 
 const MenuList = ({ menu }) => (
   <div className="flex mb-4">
@@ -35,20 +17,35 @@ const MenuList = ({ menu }) => (
   </div>
 );
 
-export const Home = () => (
-  <div className="px-4">
-    <h1 className="text-3xl text-center mt-4">ร้านอาหารครัวคุณบิน</h1>
-    <p className="mt-2">
-      ร้านอาหารครัวคุณบินปรุงด้วยใจ เหมือนทำให้คนในครอบครัวทาน
-      เราเปิดให้บริการตั้งแต่ปีพ.ศ. 2535 กว่า 30 ปีที่เรานำเสนอความ
-      อร่อยแบบไทยแท้ เราคัดสรรวัตถุดิบชั้นดี ปลอดภัย ออร์แกนิค จาก
-      เกษตรกรในพื้นที่สุโขทัย ปรุงด้วยสูตรลับต้นตำรับชาววังที่สืบทอด
-      กันมารุ่นสู่รุ่น จัดแต่งมาบนจาน พร้อมเสิร์ฟความอร่อยให้กับคุณ
-    </p>
-    <img src={Banner} className="w-full mt-4 rounded-lg" />
-    <h4 className="mt-4 mb-2 font-bold">รายการแนะนำ</h4>
-    {foodMenus.map((menu) => (
-      <MenuList menu={menu} />
-    ))}
-  </div>
-);
+export const Home = () => {
+  const getMenuUrl = process.env.REACT_APP_API_URL + "/get-menus";
+  const [foodMenus, setFoodMenus] = useState([]);
+
+  useEffect(() => {
+    const fetchMenus = async () => {
+      const result = await axios.post(getMenuUrl);
+      console.log(result.data);
+      setFoodMenus(result.data);
+    }
+
+    fetchMenus();
+  }, []);
+  
+  return (
+    <div className="px-4">
+      <h1 className="text-3xl text-center mt-4">ร้านอาหารครัวคุณบิน</h1>
+      <p className="mt-2">
+        ร้านอาหารครัวคุณบินปรุงด้วยใจ เหมือนทำให้คนในครอบครัวทาน
+        เราเปิดให้บริการตั้งแต่ปีพ.ศ. 2535 กว่า 30 ปีที่เรานำเสนอความ
+        อร่อยแบบไทยแท้ เราคัดสรรวัตถุดิบชั้นดี ปลอดภัย ออร์แกนิค จาก
+        เกษตรกรในพื้นที่สุโขทัย ปรุงด้วยสูตรลับต้นตำรับชาววังที่สืบทอด
+        กันมารุ่นสู่รุ่น จัดแต่งมาบนจาน พร้อมเสิร์ฟความอร่อยให้กับคุณ
+      </p>
+      <img src={Banner} className="w-full mt-4 rounded-lg" />
+      <h4 className="mt-4 mb-2 font-bold">รายการแนะนำ</h4>
+      {foodMenus.map((menu) => (
+        <MenuList menu={menu} />
+      ))}
+    </div>
+  );
+};
