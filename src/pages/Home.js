2dd2024ae91fbbd2b40091ce58caf3d32b58075b
Panasun Sunanta <panasunsunanta@gmail.com>
2023-02-03 10:42:57 +0700

    fix: update cart

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -47,21 +47,39 @@ const MenuSection = ({
 );
 
 const updateCart = ({ sign, cart, id, name, price }) => {
-  if (cart.some((item) => item.id === id)) {
-    return cart.reduce((arr, c) => {
-      if (c.id === id) {
-        arr.push({
-          id,
-          quantity: sign + c.quantity,
-          name,
-          price,
-          totalPrice: (sign + c.quantity) * price
-        });
-      } else {
-        arr.push(c);
-      }
-      return arr;
-    }, []);
+  // if (cart.some((item) => item.id === id)) {
+  //   return cart.reduce((arr, c) => {
+  //     if (c.id === id) {
+  //       arr.push({
+  //         id,
+  //         quantity: sign + c.quantity,
+  //         name,
+  //         price,
+  //         totalPrice: (sign + c.quantity) * price
+  //       });
+  //     } else {
+  //       arr.push(c);
+  //     }
+  //     return arr;
+  //   }, []);
+  // } else {
+  //   return [
+  //     ...cart,
+  //     {
+  //       id,
+  //       quantity: 1,
+  //       name,
+  //       price,
+  //       totalPrice: price
+  //     },
+  //   ];
+  // }
+
+  const index = cart.findIndex((r) => r.id === id);
+  if (index !== -1) {
+    const newCart = [...cart];
+    newCart[index].quantity += sign;
+    return newCart.filter((r) => r.quantity > 0);
   } else {
     return [
       ...cart,
@@ -70,10 +88,11 @@ const updateCart = ({ sign, cart, id, name, price }) => {
         quantity: 1,
         name,
         price,
-        totalPrice: price
+        totalPrice: price,
       },
     ];
   }
+
 };
 
 export const Home = () => {
